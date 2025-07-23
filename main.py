9de8f959607f3d7c28f3de2b5be0accd2fd0bf9b
unknown <Administrator@B2-2-PC1.bcmedan.com>
2025-07-23 17:33:57 +0700

    perbaikan

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ bil2=int(input("Masukkan Bilangan 2 : "))
 hasil=matematika.tambah(bil1,bil2)
 print("Hasil Tambah : ",hasil)
 
-tugas=int(input("Nilai tugas : "))
+tugas=int(input("Nilai tugas Anda: "))
 mid=int(input("Nilai Mid : "))
 uas=int(input("Nilai UAS : "))
 na=nilai.proses(tugas,mid,uas)
